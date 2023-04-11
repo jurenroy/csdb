f939b0dc6e2fc15e4ff433a7f2b76a3814e7836b
jurenroy <jurenroyabragn91019@gmail.com>
2023-04-12 00:13:10 +0800

    OB_DB

---
+++ OB_DB/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ObDbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'OB_DB'
---
+++ OB_DB/models.py
@@ -0,0 +1,27 @@
+# Create your models here.
+
+# In models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
+    birthday = models.DateField(blank=True, null=True)
+    password = models.CharField(max_length=128, blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True)
+
+class Chat(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
