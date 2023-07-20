b83416d8b4189211760cb310b7ad9cd834f13507
= <jonathanednilan@gmail.com>
2023-07-20 19:22:43 +0800

    mao ning kagabie

--- scheduler/accounts/admin.py
+++ scheduler/accounts/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import User
+from scheduling.models import Course, Room
 
 admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Room)
